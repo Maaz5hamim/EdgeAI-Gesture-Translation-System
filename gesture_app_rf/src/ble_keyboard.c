c120c9080ceaf9aba601bab5ba4ca25a66270e98
Maaz Shamim <131349204+Maaz5hamim@users.noreply.github.com>
2026-05-05 12:20:35 -0400

    Drivers added

--- gesture_app_rf/src/ble_keyboard.c
+++ gesture_app_rf/src/ble_keyboard.c
@@ -59,7 +59,7 @@ typedef struct {
 #define DEFAULT_CONFIDENCE_THRESHOLD  0.7f
 #define DEFAULT_DEBOUNCE_MS          200
 #define DEFAULT_KEY_HOLD_MS           50
-#define DEFAULT_KEY_GAP_MS            25
+#define DEFAULT_KEY_GAP_MS            10
 #define DEFAULT_KEY_REPEAT_COUNT       10
 #define DEFAULT_KEY_REPEAT_COUNT_VERT  7
 #define DEFAULT_KEY_REPEAT_COUNT_HORIZ  1
